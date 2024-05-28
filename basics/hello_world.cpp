920b938ded8f1afa7d4116915f2a612a73b514a7
Kashish Goyal <kashish1782004@gmail.com>
2024-05-29 00:03:32 +0530

    its my first time

--- basics/hello_world.cpp
+++ basics/hello_world.cpp
@@ -1,21 +1,6 @@
-#include <iostream>
-
-long long sumNaturalAdders(int n) {
-    long long sum = 0;
-    for (int i = 1; ; i++) {
-        long long triangularNumber = i * (i + 1) / 2;
-        if (triangularNumber > n) {
-            break;
-        }
-        sum += triangularNumber;
-    }
-    return sum;
-}
-
-int main() {
-    int n;
-    std::cin >> n;
-    long long result = sumNaturalAdders(n);
-    std::cout << result << std::endl;
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World";
     return 0;
 }
---
+++ basics/varaible.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int  main(){
+    int num;
+    cout<<"Enter a number";
+    cin>>num;
+    cout<<"The number you entered is:"<<num;
+    return 0;
+}
