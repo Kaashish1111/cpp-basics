2cf6ca6cb3d35fd394f98bc69bbcf836cf887943
Kashish Goyal <kashish1782004@gmail.com>
2024-05-30 21:58:45 +0530

    updated hello_world.cpp

--- basics/hello_world.cpp
+++ basics/hello_world.cpp
@@ -2,5 +2,7 @@
 using namespace std;
 int main(){
     cout<<"Hello World";
+    cout<<endl;
+    cout<<"Kashish tussi great ho"<<endl;;
     return 0;
 }
--- basics/varaible.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int  main(){
-    int num;
-    cout<<"Enter a number";
-    cin>>num;
-    cout<<"The number you entered is:"<<num;
-    return 0;
-}
