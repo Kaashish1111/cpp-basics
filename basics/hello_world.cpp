f5b784f633ed00dad5dea48143968a91c7123c13
Kashish Goyal <kashish1782004@gmail.com>
2024-05-30 22:00:13 +0530

    updated

--- basics/hello_world.cpp
+++ basics/hello_world.cpp
@@ -2,10 +2,7 @@
 using namespace std;
 int main(){
     cout<<"Hello World";
-<<<<<<< HEAD
     cout<<endl;
     cout<<"Kashish tussi great ho"<<endl;;
-=======
->>>>>>> 920b938ded8f1afa7d4116915f2a612a73b514a7
     return 0;
 }
