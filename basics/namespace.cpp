f227be884fa6b934109bc0e523afabdff3d25e7d
Kashish Goyal <kashish1782004@gmail.com>
2024-05-31 17:14:18 +0530

    two new files made

--- basics/namespace.cpp
+++ basics/namespace.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello user. How are you?";
+    cout<<"Hello user. How are you?"<<endl;
+    cout<<"\nI hope you are doing well.";
 }
