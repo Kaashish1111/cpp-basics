0245e827aef1ee37c8bf19857c915e543b039813
Kashish Goyal <kashish1782004@gmail.com>
2024-05-31 17:32:09 +0530

    update datatype.cpp

---
+++ basics/datatype.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    cout<<a<<endl;
+    float b=3.6;
+    cout<<b<<endl;
+    char c='A';
+    cout<<c<<endl;
+    double d=3.64545;
+    cout<<d<<endl;
+    bool f=true;
+    cout<<f<<endl;
+    return 0;
+}
