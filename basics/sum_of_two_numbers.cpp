8a03b2cc10f79698291a24db364d632d31b7792a
Kashish Goyal <kashish1782004@gmail.com>
2024-05-31 17:46:00 +0530

    sum of two numbers.cpp

---
+++ basics/sum_of_two_numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter another number: ";
+    cin >> b;
+    cout << "The sum of the two numbers is: " << a + b << endl;
+    return 0;
+}
---
+++ basics/taking_input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;   // >> is extraction operator
+    cout<<"The number you entred is:"<<num;  // << is insertion operator
+    return 0;
+}
