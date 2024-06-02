3b47905c3d40caf414b0cce1df6dd8f51718bc35
Kashish Goyal <kashish1782004@gmail.com>
2024-06-02 08:13:25 +0530

    new folder pattern printing 2

---
+++ basics/pattern_printing2/p1.cpp
@@ -0,0 +1,24 @@
+// *
+// **
+// ***
+// ****
+// *****
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+    cout << "Enter no of rows: ";
+    cin >> row;
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
