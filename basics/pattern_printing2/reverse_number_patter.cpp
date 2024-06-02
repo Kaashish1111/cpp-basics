42630b8cc4a8f7cb5a7c2d3ff696762024e73f10
Kashish Goyal <kashish1782004@gmail.com>
2024-06-02 08:29:45 +0530

    pattern printing

---
+++ basics/pattern_printing2/reverse_number_patter.cpp
@@ -0,0 +1,26 @@
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+// 5 4 3 2 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+    cout << "Enter the number of rows: ";
+    cin >> row;
+    for (int i = 1; i <= row; i++)
+    {
+        int num = i;
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num--;
+        }
+        cout << endl;
+    }
+    return 0;
+}
