426742a094b64025ca32044d46ee7d076b0b4790
Kashish Goyal <kashish1782004@gmail.com>
2024-06-03 16:48:07 +0530

    star pettern 5

---
+++ basics/pattern_printing4/star_pettern5.cpp
@@ -0,0 +1,44 @@
+//    *
+//   * *
+//  * * *
+// * * * *
+//  * * *
+//   * *
+//    *
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+    cout << "Enter the number of rows: ";
+    cin >> row;
+    for (int i = 1; i <= row; i++)
+    {
+        int no_of_gaps = row - i;
+        for (int j = 1; j <= row - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+    for (int i = row - 1; i >= 1; i--)
+    {
+        int no_of_gaps = row - i;
+        for (int j = 1; j <= row - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
