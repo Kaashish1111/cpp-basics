cbcc841a297465aad52958eb800a0d2790e71e54
Kashish Goyal <kashish1782004@gmail.com>
2024-06-26 21:35:03 +0530

    lecture19 complted.cpp

---
+++ basics/lecture19/selection_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int index = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[index])
+            {
+                index = j;
+            }
+        }
+        swap(arr[index], arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
