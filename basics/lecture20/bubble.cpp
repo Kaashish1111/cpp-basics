f8101fbb563fc0ab47c4ff63723514bb07289baf
Kashish Goyal <kashish1782004@gmail.com>
2024-06-27 11:29:04 +0530

    updated bubble sort

--- basics/lecture20/bubble.cpp
+++ basics/lecture20/bubble.cpp
@@ -20,11 +20,16 @@ int main()
 
     for (int i = 0; i <= n - 1; i++)
     {
+        bool swapped = 0;
         for (int j = 0; j < n; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                swapped = 1;
+            }
+            if (swapped == 0){
+                break;
             }
         }
     }
